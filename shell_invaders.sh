49b40e51bdb4838842bec964a35a8c5daa0ad3d1
Daniil669 <daniil.freeman669@gmail.com>
2025-10-30 10:53:10 +0200

    spaceship moves

--- shell_invaders.sh
+++ shell_invaders.sh
@@ -1,96 +1,150 @@
 #!/bin/bash
 
 #TODO
-#0 Make home screen with ASCII Shell Invaders, press "Space" to start; make game field, display score, lives;
-#1 Show ship; Make it move and update screen;
 #2 Show enemy; Make ship to shoot; Make enemy disappear;
 #3 Make enemy move from left to right and go down and then go from right to left do this until it hits the enemy or killed;
 #4 Make multiple enemies; Add score when enemy is hit;
 #5 Make enemy to shoot; If starship is hit, decrease remaining lives;
 #6 When no lives left, print GAME OVER!; When no enemies left, spawn them again at the top.
 
-#shell invaders like space invaders but in shell
-game_name="Shell Invaders"
 
 ascii_art=(" " "_" "_" "_" "_" "_" " " "_" " " " " " " " " " " " " " " " " " " " " "_" " " "_" " " " " " " "_" "_" "_" "_" "_" " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "_" " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "/" " " " " "_" "_" "_" "|" " " "|" " " " " " " " " " " " " " " " " "|" " " "|" " " "|" " " "|" "_" " " " " " " "_" "|" " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " " "|" " " "|" " " " " " " " " " " " " " " " " " " " " " " " " " " " " "\\" " " "\`" "-" "-" "." "|" " " "|" "_" "_" " " " " " " "_" "_" "_" "|" " " "|" " " "|" " " " " " " "|" " " "|" " " "_" " " "_" "_" "_" "_" " " " " " " "_" "_" "_" "_" " " "_" " " " " "_" "_" "|" " " "|" " " "_" "_" "_" " " "_" " " "_" "_" " " "_" "_" "_" " " " " "\`" "-" "-" "." " " "\\" " " "'" "_" " " "\\" " " "/" " " "_" " " "\\" " " "|" " " "|" " " " " " " "|" " " "|" "|" " " "'" "_" " " "\\" " " "\\" " " "/" " " "/" " " "_" "\`" " " "|" "/" " " "_" "\`" " " "|" "/" " " "_" " " "\\" " " "'" "_" "_" "/" " " "_" "_" "|" "/" "\\" "_" "_" "/" " " "/" " " "|" " " "|" " " "|" " " " " "_" "_" "/" " " "|" " " "|" " " " " "_" "|" " " "|" "|" " " "|" " " "|" " " "\\" " " "V" " " "/" " " "(" "_" "|" " " "|" " " "(" "_" "|" " " "|" " " " " "_" "_" "/" " " "|" " " " " "\\" "_" "_" " " "\\" "\\" "_" "_" "_" "_" "/" "|" "_" "|" " " "|" "_" "|" "\\" "_" "_" "_" "|" "_" "|" "_" "|" " " " " "\\" "_" "_" "_" "/" "_" "|" " " "|" "_" "|" "\\" "_" "/" " " "\\" "_" "_" "," "_" "|" "\\" "_" "_" "," "_" "|" "\\" "_" "_" "_" "|" "_" "|" " " " " "|" "_" "_" "_" "/")
 
 start_string=('p' 'r' 'e' 's' 's' ' ' 'S' ' ' 't' 'o' ' ' '[' 'S' 'T' 'A' 'R' 'T' ']')
 ending_string=('p' 'r' 'e' 's' 's' ' ' 'F' ' ' 't' 'o' ' ' '[' 'F' 'I' 'N' 'I' 'S' 'H' ']')
-score_string=('s' 'c' 'o' 'r' 'e' ':' ' ')
-lives_string=('l' 'i' 'v' 'e' 's' ':' ' ')
-
+score_string=('s' 'c' 'o' 'r' 'e' ':' ' ') #l=7
+lives_string=('l' 'i' 'v' 'e' 's' ':' ' ') #l=7
+shoot_string=('s' ' ' '-' ' ' 's' 'h' 'o' 'o' 't') #l=9
+move_string=('j' ' ' '-' ' ' 'm' 'o' 'v' 'e' ' ' 'l' 'e' 'f' 't' ';' ' ' 'l' ' ' '-' ' ' 'm' 'o' 'v' 'e' ' ' 'r' 'i' 'g' 'h' 't') #l=28
 
 lives=3
 enemies=1
-score=0
+score=420
 
 art_char_index=0
 start_string_index=0
 ending_string_index=0
 score_string_index=0
 lives_string_index=0
-score_length=1
+shoot_string_index=0
+move_string_index=0
+
+ship_position=32 #3<=sp<=67
+
+score_length=0
 
 draw_home_screen() {
-for i in {1..36}; do
-for j in {1..69}; do
-if [ $i -eq 1 ] || [ $i -eq 36 ]; then
-echo -n "*"
-elif [ $j -eq 1 ] || [ $j -eq 69 ]; then
-echo -n "*"
-elif [ $i -ge 15 ] && [ $i -le 20 ] && [ $j -ge 3 ] && [ $j -le 67 ]; then
-echo -n "${ascii_art[$char_index]}"
-((char_index++))
-elif [ $i -eq 29 ] && [ $j -ge 25 ] && [ $j -le 42 ]; then
-echo -n "${start_string[$start_string_index]}"
-((start_string_index++))
-elif [ $i -eq 32 ] && [ $j -ge 25 ] && [ $j -le 43 ]; then
-echo -n "${ending_string[$ending_string_index]}"
-((ending_string_index++))
-else
-echo -n " "
-fi
-done
-echo ""
-done
+    for i in {1..36}; do
+        for j in {1..69}; do
+            if [ $i -eq 1 ] || [ $i -eq 36 ]; then
+                echo -n "*"
+            elif [ $j -eq 1 ] || [ $j -eq 69 ]; then
+                echo -n "*"
+            elif [ $i -ge 15 ] && [ $i -le 20 ] && [ $j -ge 3 ] && [ $j -le 67 ]; then
+                echo -n "${ascii_art[$char_index]}"
+                ((char_index++))
+            elif [ $i -eq 29 ] && [ $j -ge 25 ] && [ $j -le 42 ]; then
+                echo -n "${start_string[$start_string_index]}"
+                ((start_string_index++))
+            elif [ $i -eq 32 ] && [ $j -ge 25 ] && [ $j -le 43 ]; then
+                echo -n "${ending_string[$ending_string_index]}"
+                ((ending_string_index++))
+            else
+                echo -n " "
+            fi
+        done
+        echo ""
+    done
 }
 
 game() {
-for i in {1..36}; do
-score_string_index=0
-lives_string_index=0
-for j in {1..69}; do
-if [ $i -eq 1 ] || [ $i -eq 36 ]; then
-echo -n "*"
-elif [ $i -eq 3 ] && [ $j -ge 3 ] && [ $j -le ((9 + $score_length)) ]; then
-echo -n "${score_string[$score_string_index]}"
-(($score_string_index++))
-elif [ $i -eq 5 ] && ||  [ $i -eq 32 ] && [ $j -ge 3 ] && [ $j -le 67 ]; then
-echo -n "-"
-elif [ $j -eq 1 ] || [ $j -eq 69 ]; then
-echo -n "*"
-else
-echo -n " "
-fi
-done
-echo ""
-done
+    while true; do
+        tput clear
+        for i in {1..36}; do
+        score_string_index=0
+        lives_string_index=0
+        move_string_index=0
+        shoot_string_index=0
+            if [ $score -gt 9 ] && [ $score -lt 100 ]; then
+                score_length=2
+            elif [ $score -gt 99 ] && [ $score -le 999 ]; then
+                score_length=3
+            else
+                score_length=1
+            fi
+            for j in {1..69}; do
+                if [ $i -eq 1 ] || [ $i -eq 36 ]; then
+                    echo -n "*"
+                elif [ $i -eq 3 ] && [ $j -ge 3 ] && [ $j -le $((9 + $score_length)) ]; then
+                    if [ $score_string_index -eq 8 ]; then
+                        continue
+                    fi
+                    if [ $score_string_index -eq 7 ]; then
+                        echo -n "$score"
+                        ((score_string_index++))
+                        continue
+                    fi
+                    echo -n "${score_string[$score_string_index]}"
+                    ((score_string_index++))
+                elif [ $i -eq 3 ] && [ $j -ge 60 ] && [ $j -le 67 ]; then
+                    if [ $lives_string_index -eq 7 ]; then
+                        echo -n "$lives"
+                        continue
+                    fi
+                    echo -n "${lives_string[$lives_string_index]}"
+                    ((lives_string_index++))
+                elif [ $i -eq 5 ] ||  [ $i -eq 32 ] && [ $j -ge 3 ] && [ $j -le 67 ]; then
+                    echo -n "-"
+                elif [ $i -eq 34 ] && [ $j -ge 3 ] && [ $j -le 11 ]; then
+                    echo -n "${shoot_string[$shoot_string_index]}"
+                    ((shoot_string_index++))
+                elif [ $i -eq 34 ] && [ $j -ge 40 ] && [ $j -le 67 ]; then
+                    echo -n "${move_string[$move_string_index]}"
+                    ((move_string_index++))
+                elif [ $i -eq 30 ] && [ $j -eq $ship_position ]; then
+                    echo -n "A"
+                elif [ $j -eq 1 ] || [ $j -eq 69 ]; then
+                    echo -n "*"
+                else
+                    echo -n " "
+                fi
+            done
+        echo ""
+        done
+        read -n 1 -s action
+        if [[ "$action" == "s" ]] || [[ "$action" == "S" ]]; then
+            echo "Shoot"
+        elif [[ "$action" == "j" ]] || [[ "$action" == "J" ]]; then
+            if [ $ship_position -gt 3 ]; then
+                ((ship_position--))
+            fi
+        elif [[ "$action" == "l" ]] || [[ "$action" = "L" ]]; then
+            if [ $ship_position -lt 67 ]; then
+                ((ship_position++))
+            fi
+        elif [[ "$action" == "f" ]] || [[ "$action" == "F" ]]; then
+            echo "Game Over!"
+            break
+        fi
+    done
 }
 
 main() {
-tput setaf 2
-draw_home_screen
-while true; do
-read -n 1 -s key
-if [[ "$key" == "s" ]] || [[ "$key" == "S" ]]; then
-game
-break
-elif [[ "$key" == "F" ]] || [[ "$key" == "f" ]]; then
-echo "Bye!"
-break
-fi
-done
-tput sgr0
+    tput clear
+    tput setaf 2
+    draw_home_screen
+    while true; do
+        read -n 1 -s key
+        if [[ "$key" == "s" ]] || [[ "$key" == "S" ]]; then
+            game
+            break
+        elif [[ "$key" == "F" ]] || [[ "$key" == "f" ]]; then
+            tput clear
+            echo "YOU SHELL NOT BE INVADED!"
+            break
+        fi
+    done
+    tput sgr0
 }
 
 main
